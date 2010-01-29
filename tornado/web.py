2db1609abcb2d14412cab2986fffeb7b2886474b
Milan Cermak <milan.cermak@gmail.com>
2010-01-29 15:58:16 +0100

    Refactored session creation.

--- tornado/web.py
+++ tornado/web.py
@@ -790,21 +790,20 @@ class RequestHandler(object):
     def _create_session(self):
         settings = self.application.settings # just a shortcut
         url = settings.get('session_storage', 'file://') # default to file storage
-        expires = settings.get('session_age')
-        session_id = self.get_secure_cookie(settings.get('session_cookie_name', 'session_id'))
+        kw = {'session_id': self.get_secure_cookie(settings.get('session_cookie_name', 'session_id')),
+              'security_model': settings.get('session_security_model', []),
+              'expires': settings.get('session_age', 900),
+              'ip_address': self.request.remote_ip,
+              'user_agent': self.request.headers.get('User-Agent')
+              }
         new_session = None
         old_session = None
 
         if url and not url.startswith('file'):
             if url.startswith('mysql'):
-                old_session = session.MySQLSession.load(session_id, settings['_db'])
+                old_session = session.MySQLSession.load(kw['session_id'], settings['_db'])
                 if old_session is None: # create a new session
-                    new_session = session.MySQLSession(
-                        settings['_db'],
-                        security_model=settings.get('session_security_model', []),
-                        expires=expires,
-                        ip_address=self.request.remote_ip,
-                        user_agent=self.request.headers.get('User-Agent'))
+                    new_session = session.MySQLSession(settings['_db'], **kw)
             elif url.startswith('postgresql'):
                 raise NotImplemented
             elif url.startswith('sqlite'):
@@ -815,24 +814,14 @@ class RequestHandler(object):
                 raise NotImplemented
             elif url.startswith('dir'):
                 dir_path = url[6:]
-                old_session = session.DirSession.load(session_id, dir_path)
+                old_session = session.DirSession.load(kw['session_id'], dir_path)
                 if old_session is None: # create new session
-                    new_session = session.DirSession(
-                        dir_path,
-                        security_model=settings.get('session_security_model', []),
-                        expires=expires,
-                        ip_address=self.request.remote_ip,
-                        user_agent=self.request.headers.get('User-Agent'))
+                    new_session = session.DirSession(dir_path, **kw)
         else:
-            path = url[7:]
-            old_session = session.FileSession.load(session_id, path)
+            file_path = url[7:]
+            old_session = session.FileSession.load(kw['session_id'], file_path)
             if old_session is None: # create new session
-                new_session = session.FileSession(
-                    security_mode=settings.get('session_security_model', []),
-                    expires=expires,
-                    ip_address=self.request.remote_ip,
-                    user_agent=self.request.headers.get('User-Agent'),
-                    file_path=path)
+                new_session = session.FileSession(file_path, **kw)
 
         if old_session is not None:
             # TODO: security checks, session regeneration
